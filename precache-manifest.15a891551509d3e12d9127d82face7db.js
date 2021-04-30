f8cee1a69020c6cae17b12ae447f1ae6727767c2
harris012 <harissaeed39@yahoo.com>
2021-04-30 07:47:20 +0200

    Updates

---
+++ precache-manifest.15a891551509d3e12d9127d82face7db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6cf98c6b9897fd56b67f9844032bc11",
+    "url": "/menu/index.html"
+  },
+  {
+    "revision": "0e311feff4865ac1ddaf",
+    "url": "/menu/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/menu/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menu/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e311feff4865ac1ddaf",
+    "url": "/menu/static/js/main.916bfa5a.chunk.js"
+  },
+  {
+    "revision": "56ef0208991cca3c548d",
+    "url": "/menu/static/js/runtime-main.a5a74f46.js"
+  }
+]);
